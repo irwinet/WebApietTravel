819f00d9562202a75cafbd3e9dfa436929d74e12
irwinet <32725479+irwinet@users.noreply.github.com>
2017-12-19 23:09:39 -0500

    Change
    Avance Donde Ir  View

--- donde_ir.php
+++ donde_ir.php
@@ -35,17 +35,17 @@
 		</div>
 		<div class="col-md-12">
 			<div class="row">
-			  <div class="col-4">
+			  <div class="col-sm-12 col-md-4">
 			    <div class="list-group" id="list-tab" role="tablist">
-			      <a class="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home"><span class="icon icon-file-text2"></span> Datos Útiles</a>
-			      <a class="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Patrimonio Cultural</a>
-			      <a class="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Atractivos Dentro de la Ciudad</a>
-			      <a class="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Atratacticos Fuera de la Ciudad</a>
+			      <a class="list-group-item list-group-item-action active" id="list-datos-list" data-toggle="list" href="#list-datos" role="tab" aria-controls="datos"><span class="icon icon-file-text2"></span> Datos Útiles</a>
+			      <a class="list-group-item list-group-item-action" id="list-dentro-list" data-toggle="list" href="#list-dentro" role="tab" aria-controls="dentro"><span class="icon icon-location2"></span> Atractivos Dentro de la Ciudad</a>
+			      <a class="list-group-item list-group-item-action" id="list-fuera-list" data-toggle="list" href="#list-fuera" role="tab" aria-controls="fuera"><span class="icon icon-map"></span> Atratacticos Fuera de la Ciudad</a>
 			    </div>
 			  </div>
-			  <div class="col-8">
+			  <div class="col-sm-12 col-md-8">
 			    <div class="tab-content" id="nav-tabContent">
-			      <div class="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">
+			      <div class="tab-pane fade show active" id="list-datos" role="tabpanel" aria-labelledby="list-datos-list">
+			      	<!-- Apurimac -->
 			      	<h1>Apurímac <span class="badge badge-primary">Corazón Andino</span></h1>
 					<p class="text-justify">
 						<strong>Apurímac</strong> reúne aguas termales, bosques cordilleranos, lagunas, flora, fauna, un enorme nevado (Ampay) y el profundo Cañón Apurímac, límite natural con el Cusco y escenario de deportes de aventura. Las huellas de un pasado inca y colonial se reúnen dentro de la arquitectura mestiza y las permanentes fiestas costumbristas alrededor de las distintas ciudades de Abancay, Andahuaylas y entre otras.
@@ -59,19 +59,197 @@
 					<div class="embed-responsive embed-responsive-16by9">
 					  <iframe width="560" height="315" src="https://www.youtube.com/embed/tgydgeyBY-w" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
 					</div>
+					<hr>
+					<h1>Clima <span class="badge badge-primary">Templado</span></h1>
+					<div class="row">
+						<div class="col-md-12">
+							<img src="img/donde_ir/clima.jpg" class="rounded mx-auto d-block" alt="Clima">
+							<br>
+						</div>						
+						<div class="col-sm-12 col-md-6">
+							<p class="text-justify">Temperatura máxima</p>
+							<p class="clima text-justify"><strong>23,8&deg;C</strong></p>
+							<p class="text-justify"><strong>74,8&deg;F</strong></p>
+						</div>
+						<div class="col-sm-12 col-md-6">
+							<p class="text-justify">Temperatura mínima</p>
+							<p class="clima text-justify"><strong>11,7&deg;C</strong></p>
+							<p class="text-justify"><strong>53&deg;F</strong></p>
+						</div>
+					</div>
+					<hr>
+					<h1>Vias de Acceso <span class="badge badge-primary">Aérea - Terrestre</span></h1>
+					<h2>Vía de Acceso Aérea</h2>
+					<p class="text-justify">
+						<strong>Lima-Cusco</strong> (1 hora), <strong>Cusco-Abancay</strong> (4 horas vía terrestre). <strong>Lima-Andahuaylas:</strong> (1 hora) <strong>Andahuaylas - Abancay</strong> (4 horas - vía terrestre).
+					</p>
+					<h2>Vía de Acceso Terrestre</h2>
+					<div class="row">
+						<div class="col-sm-12 col-md-6">
+							<p class="via">Lima-Nasca-Puquio-Abancay: <strong>912 Km</strong></p>
+							<p class="font-weight-light">(18 horas en bus).</p>
+							<p class="via">Cusco-Abancay: <strong>198 Km</strong></p>
+							<p class="font-weight-light">(4 horas en bus).</p>
+							<p class="via">Abancay-Cachora: <strong>63 Km</strong></p>
+							<p class="font-weight-light">(2 horas en bus).</p>
+							<p class="via">Abancay-Huanipaca: <strong>42 Km</strong></p>
+							<p class="font-weight-light">(2 horas en bus).</p>
+						</div>
+						<div class="col-sm-12 col-md-6">
+							<p class="via">Lima-Ayacucho-Chincheros-Andahuaylas-Abancay: <strong>956 Km</strong></p>
+							<p class="font-weight-light">(19 horas en bus).</p>
+							<p class="via">Abancay-Curahuasi: <strong>73 Km</strong></p>
+							<p class="font-weight-light">(4 horas en bus).</p>
+							<p class="via">Abancay-Saywite: <strong>48 Km</strong></p>
+							<p class="font-weight-light">(45 minutos en bus).</p>
+						</div>
+					</div>
+					<hr>
+					<h1>Distancias <span class="badge badge-primary">Abancay</span></h1>
+					<div class="row">
+						<div class="col-md-12">
+							<img src="img/donde_ir/distancia.jpg" class="rounded mx-auto d-block" alt="Distancia">
+							<br>
+						</div>	
+						<div class="col-sm-12 col-md-6">
+							<h3 class="distancias">Andahuaylas</h3>
+							<p class="distancias">(Provincia de Andahuaylas)</p>
+							<p>121 Km / 3 horas 30 min en auto</p>			
+							<h3 class="distancias">Chalhuanca</h3>
+							<p class="distancias">(Provincia de Aymaraes)</p>
+							<p>120 Km / 2 horas en bus</p>
+							<h3 class="distancias">Chincheros</h3>
+							<p class="distancias">(Provincia de Chincheros)</p>
+							<p>226 Km / 5 horas y 30 minutos</p>
+						</div>
+						<div class="col-sm-12 col-md-6">
+							<h3 class="distancias">Antabamba</h3>
+							<p class="distancias">(Provincia de Antabamba)</p>
+							<p>144 Km / 5 horas en auto</p>			
+							<h3 class="distancias">Tambobamba</h3>
+							<p class="distancias">(Provincia de Cotabambas)</p>
+							<p>248 Km / 11 horas</p>
+							<h3 class="distancias">Chuquibambilla</h3>
+							<p class="distancias">(Provincia de Grau)</p>
+							<p>122 Km / 4 horas en auto</p>
+						</div>
+					</div>
+					<hr>
+					<h1>Altitud</h1>
+					<div class="row">
+						<div class="col-md-12">
+							<img src="img/donde_ir/altitud.jpg" class="rounded mx-auto d-block" alt="Altitud">
+							<br>
+						</div>	
+						<div class="col-sm-12 col-md-6">
+							<p class="altitud text-justify">Mínima</p>
+							<p style="color: #112b7a;" class="text-justify"><strong>2.378 msnm (Abancay)</strong></p>
+						</div>
+						<div class="col-sm-12 col-md-6">
+							<p class="altitud text-justify">Máxima</p>
+							<p style="color: #112b7a;" class="text-justify"><strong>2.378 msnm (Abancay)</strong></p>
+						</div>
+					</div>
 			      </div>
 
-			      <div class="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
-			      	<h1>Arqueologia</h1>
+			      <div class="tab-pane fade" id="list-dentro" role="tabpanel" aria-labelledby="list-dentro-list">
+			      	<div class="row">
+						<ul class="list-unstyled">
+						  <li class="media">
+						    <img class="mr-3 rounded" width="200px" height="150px" src="img/donde_ir/plaza_mayor.jpg" alt="Plaza Mayor">
+						    <div class="media-body">
+						      <h5 class="mt-0 mb-1">Plaza Mayor</h5>
+						      <p class="text-justify">Acogedora plaza de la ciudad. Con imágenes en bronce que representan la fauna silvestre local: el cóndor, el venado y el puma.</p>
+						    </div>
+						  </li>
+						  <li class="media my-4">
+						    <img class="mr-3 rounded" width="200px" height="150px" src="img/donde_ir/casona_illanya.jpg" alt="Casona Illanya">
+						    <div class="media-body">
+						      <h5 class="mt-0 mb-1">Casona Illanya</h5>
+						      <p class="text-justify">Una primeras haciendas que surge a inicios de la colonia (1592). Levantada con elegante estilo arquitectónico francés. Produjo aguardiente de caña de azúcar, chancaca, miel de caña y contaba con moneda propia. Actualmente se encuentra restaurada, en ella se puede visitar el museo del Instituto Nacional de Cultura de Apurímac.</p>
+						    </div>
+						  </li>
+						  <li class="media">
+						    <img class="mr-3 rounded" width="200px" height="150px" src="img/donde_ir/mirador.jpg" alt="Parque Recreacional Taraccas (El Mirador)">
+						    <div class="media-body">
+						      <h5 class="mt-0 mb-1">Parque Recreacional Taraccas (El Mirador)</h5>
+						      <p class="text-justify">Se observa la espléndida ciudad de Abancay, además de disfrutar de un paseo por el zoológico con animales de la región.</p>
+						    </div>
+						  </li>
+						</ul>	
+			      	</div>
 			      </div>
 
-			      <div class="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">
-			      	<h1>Plaza de Armas</h1>
+			      <div class="tab-pane fade" id="list-fuera" role="tabpanel" aria-labelledby="list-fuera-list">
+			      	<div class="row">
+						<ul class="list-unstyled">
+						  <li class="media">
+						    <img class="mr-3 rounded" width="200px" height="150px" src="img/donde_ir/ampay.jpg" alt="Santuario Nacional de Ampay (Abancay)">
+						    <div class="media-body">
+						      <h5 class="mt-0 mb-1">Santuario Nacional de Ampay (Abancay)</h5>
+						      <p class="text-justify">Protege principalmente la intimpa (podocarpus glomeratus don), especie arbórea en peligro de extinción, además de flora como las orquídeas, las bromelias y los helechos. Entre la fauna destacan los zorros, venados, osos de anteojos, vizcachas, pumas, gatos monteses, zorrinos, perdices y búhos. Área protegida con una extensión de <strong>3.635,5 hectáreas</strong> y una altura entre los <strong>2.850 msnm</strong> y <strong>5.200 msnm.</strong></p>
+						    </div>
+						  </li>
+						  <li class="media my-4">
+						    <img class="mr-3 rounded" width="200px" height="150px" src="img/donde_ir/cconoc.jpg" alt="Baños Termales de Cconoc (Abancay)">
+						    <div class="media-body">
+						      <h5 class="mt-0 mb-1">Baños Termales de Cconoc (Abancay)</h5>
+						      <p class="text-justify">Localizados al pie del río Apurímac y rodeados de algarrobos, carrizales y cactus. Los baños, según se dice, curan enfermedades como la artritis, los dolores musculares y el reumatismo, entre otros. Sus aguas son inodoras, de agradable sabor salino.</p>
+						    </div>
+						  </li>
+						  <li class="media">
+						    <img class="mr-3 rounded" width="200px" height="150px" src="img/donde_ir/cañon.jpg" alt="Cañón del Río Apurímac (Abancay)">
+						    <div class="media-body">
+						      <h5 class="mt-0 mb-1">Cañón del Río Apurímac (Abancay)</h5>
+						      <p class="text-justify">Límite entre las regiones de Apurímac y Cusco, uno de los más profundos de América y posiblemente del mundo. La quebrada honda, en la ruta Abancay-Cusco, es el lugar donde mejor se observa su gran belleza. El río baja violentamente desde los 5.000 msnm, hacia una gigantesca garganta, un impresionante espectáculo de la naturaleza. Es ideal para practicar canotaje, con rápidos de categoría <strong>II, III y IV.</strong></p>
+						    </div>
+						  </li>
+						  <li class="media">
+						    <img class="mr-3 rounded" width="200px" height="150px" src="img/donde_ir/saywite.jpg" alt="Conjunto Arqueológico de Saywite (Abancay)">
+						    <div class="media-body">
+						      <h5 class="mt-0 mb-1">Conjunto Arqueológico de Saywite (Abancay)</h5>
+						      <p class="text-justify">Conjunto arqueológico donde destaca el Monolito de Saywite o monolito principal que mide <strong>2,5</strong> metros de altura, con tallados que representan la geografía y el medio ambiente de la zona. Dentro del complejo <strong>(2.400 msnm)</strong> puede observarse una sucesión de nueve fuentes, así como una serie de monolitos menores asociados a otro conjunto de fuentes; un altar con una plataforma rectangular de <strong>18</strong> por <strong>34</strong> metros vinculado al culto solar y la observación astronómica; resaltando el Intihuatana, bloque de piedra tallada con formas diversas.</p>
+						    </div>
+						  </li>
+						  <li class="media">
+						    <img class="mr-3 rounded" width="200px" height="150px" src="img/donde_ir/andahuaylas.jpg" alt="Ciudad de Andahuaylas (Andahuaylas)">
+						    <div class="media-body">
+						      <h5 class="mt-0 mb-1">Ciudad de Andahuaylas (Andahuaylas)</h5>
+						      <p class="text-justify">Su iglesia principal es la Catedral de San Pedro y tiene una arquitectura colonial. En la Plaza de Armas destaca la pileta construida de una sola piedra.</p>
+						    </div>
+						  </li>
+						  <li class="media">
+						    <img class="mr-3 rounded" width="200px" height="150px" src="img/donde_ir/chumbao.jpg" alt="Puente Colonial El Chumbao (Andahuaylas)">
+						    <div class="media-body">
+						      <h5 class="mt-0 mb-1">Puente Colonial El Chumbao (Andahuaylas)</h5>
+						      <p class="text-justify">Las construcciones pertenecieron originalmente a la cultura Huari (600-900 d.C.), pero fueron posteriormente ocupadas por los incas. El complejo cuenta con 10 hectáreas de extensión, de forma piramidal, rodeada por muros y terrazas que desembocan en plataformas con amplios espacios abiertos. Aparentemente, en Sondor (3.200 msnm) se habrían desarrollado actividades religiosas por la ubicación y la forma de la pirámide central, con una visión estratégica hacia la laguna de Pacucha.</p>
+						    </div>
+						  </li>
+						  <li class="media">
+						    <img class="mr-3 rounded" width="200px" height="150px" src="img/donde_ir/sondor.jpg" alt="Complejo Arqueológico de Sondor (Andahuaylas)">
+						    <div class="media-body">
+						      <h5 class="mt-0 mb-1">Complejo Arqueológico de Sondor (Andahuaylas)</h5>
+						      <p class="text-justify">Su iglesia principal es la Catedral de San Pedro y tiene una arquitectura colonial. En la Plaza de Armas destaca la pileta construida de una sola piedra.</p>
+						    </div>
+						  </li>
+						  <li class="media">
+						    <img class="mr-3 rounded" width="200px" height="150px" src="img/donde_ir/pacucha.jpg" alt="Laguna de Pacucha (Andahuaylas)">
+						    <div class="media-body">
+						      <h5 class="mt-0 mb-1">Laguna de Pacucha (Andahuaylas)</h5>
+						      <p class="text-justify">Aguas intensamente azules (3.091 msnm), rodeada de depósitos de arena amarillenta y totorales que sirven de hábitat a diferentes especies de patos.</p>
+						    </div>
+						  </li>
+						  <li class="media">
+						    <img class="mr-3 rounded" width="200px" height="150px" src="img/donde_ir/hualalachi.jpg" alt="Aguas minerales de Hualalachi (Andahuaylas)">
+						    <div class="media-body">
+						      <h5 class="mt-0 mb-1">Aguas minerales de Hualalachi (Andahuaylas)</h5>
+						      <p class="text-justify">Conocidas por sus propiedades curativas y diuréticas. Las aguas con alto contenido de minerales ferrosos y sódicos, que sirven para el tratamiento de enfermedades gastrointestinales, así como para el relajamiento del sistema muscular.</p>
+						    </div>
+						  </li>
+						</ul>	
+			      	</div>
 			      </div>
 
-			      <div class="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">
-			      	<h1>Santuario Nacional del Ampay</h1>
-			      </div>
 			    </div>
 			  </div>
 			</div>
